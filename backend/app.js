d13432dd49de2cc30ec5e9b190b400eb3e96a396
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-10 15:21:32 +0900

    add app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
-console.log('');
+const express = require('express');
+const cors = require('cors');
+
+const port = 3001;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello, api server');
+})
+
+// サーバ待機（server listen)
+app.listen(port, () => {
+    console.log(`Server listen http://localhost:${port}`)
+});
