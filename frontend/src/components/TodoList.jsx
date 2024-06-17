e905cc7720974d8c7f31cea0a80175cf30609205
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-17 14:02:18 +0900

    update

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const defaultTodo = {
+    id: 0,
+    title: '',
+    completed: false,
+    order: 0,
+}
 
 function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState(defaultTodo);
+
     return (
         <div>
             <h2>TodoList</h2>
+            <input type="text" />
+            <button>Add</button>
         </div>
     )
 }
